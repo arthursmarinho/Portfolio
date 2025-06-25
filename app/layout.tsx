9037180c95a9eec52e80430be5d1b2bf935da9a0
arthursmarinho <arthursnat@gmail.com>
2025-06-25 20:07:56 -0300

    fix: correct language attribute in RootLayout and ensure proper Head component usage

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import NavBar from "@/components/Navbar/page";
 const inter = Inter({ subsets: ["latin"] });
 import { Analytics } from "@vercel/analytics/next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Arthur WebDev",
@@ -16,7 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" href="/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
       <body className={`${inter.className} animated-dotted-background`}>
         <Provider>
           <NavBar />
