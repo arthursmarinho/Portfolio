1dca8eae792c36f6aefe09960bfdac49bb731833
arthursmarinho <arthursnat@gmail.com>
2025-06-25 20:20:24 -0300

    fix: remove Head component and associated meta tags for cleaner layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,16 +18,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
-        <link rel="icon" href="/icon-192x192.png" />
-        <link rel="apple-touch-icon" href="/icon-192x192.png" />
-      </Head>
       <body className={`${inter.className} animated-dotted-background`}>
         <Provider>
           <NavBar />
